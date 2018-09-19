77d2c19f1d8035d375f4f04b382ba001b4a26afd
shubhamgautam01 <shubhamgautam111996@gmail.com>
2018-09-20 01:09:56 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Handle Middlewares
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Index Route
+app.get('/', (req,res) =>{
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
+
+//About Route
+app.get('/about', (req,res) =>{
+  res.render('about');
+});
+
+const port = 5000;
+app.listen(port, () =>{
+  console.log(`Server Started on posr ${port}`);
+});
